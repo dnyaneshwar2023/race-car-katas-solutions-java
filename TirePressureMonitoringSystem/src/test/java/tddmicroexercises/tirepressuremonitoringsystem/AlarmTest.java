b3492007d27fa23e940672b383305ac0ab163e91
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2024-10-03 21:10:04 +0530

    Add tyre pressure kata solution

--- TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -1,14 +1,31 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class AlarmTest {
+    Sensor sensor = Mockito.mock(Sensor.class);
 
     @Test
-    public void foo() {
-        Alarm alarm = new Alarm();
+    public void shouldBeOn() {
+        Alarm alarm = new Alarm(sensor, false, new SafetyRange(17, 21));
+
+        when(sensor.popNextPressurePsiValue()).thenReturn(10.0);
+        alarm.check();
+
+        assertTrue(alarm.isAlarmOn());
+    }
+
+    @Test
+    public void shouldBeOff() {
+        Alarm alarm = new Alarm(sensor, false, new SafetyRange(17, 21));
+
+        when(sensor.popNextPressurePsiValue()).thenReturn(18.0);
+        alarm.check();
+
         assertFalse(alarm.isAlarmOn());
     }
 }
