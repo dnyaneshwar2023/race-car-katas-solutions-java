bdabbdaf206d84b6be353a1558ea9ee151cdfdc9
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2024-10-04 17:21:32 +0530

    Add ticket dispenser solution

--- TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TurnTicket.java
+++ TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TurnTicket.java
@@ -3,15 +3,12 @@ package tddmicroexercises.turnticketdispenser;
 public class TurnTicket
 {
     private final int turnNumber;
-
     public TurnTicket(int turnNumber)
     {
         this.turnNumber = turnNumber;
     }
-
     public int getTurnNumber()
     {
         return turnNumber;
     }
-
 }
--- TurnTicketDispenser/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
+++ TurnTicketDispenser/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -7,10 +7,22 @@ import org.junit.jupiter.api.Test;
 class TicketDispenserTest {
 
     @Test
-    void foo() {
-        TicketDispenser dispenser = new TicketDispenser();
+    void getTicket() {
+        TicketDispenser dispenser = new TicketDispenser(new TurnNumberSequence());
         TurnTicket ticket = dispenser.getTurnTicket();
-        assertEquals(-1, ticket.getTurnNumber());
+
+        assertEquals(0, ticket.getTurnNumber());
     }
 
+    @Test
+    void twoDispensersShouldNotGenerateSameTicket() {
+        TurnNumberSequence sequence = new TurnNumberSequence();
+        TicketDispenser dispenser = new TicketDispenser(sequence);
+        TicketDispenser dispenser2 = new TicketDispenser(sequence);
+        TurnTicket ticket = dispenser.getTurnTicket();
+        TurnTicket ticket2 = dispenser2.getTurnTicket();
+
+        assertEquals(0, ticket.getTurnNumber());
+        assertEquals(1, ticket2.getTurnNumber());
+    }
 }
